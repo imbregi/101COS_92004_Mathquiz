def682138b2cb14ac52e92ab4723f84cb7a28b02
imbregi <bodhiandrew@mmc.school.nz>
2024-06-24 10:55:19 +1200

    Add files via upload

--- math_quiz_v1.py
+++ math_quiz_v1.py
@@ -5,10 +5,11 @@ correct = 0
 incorrect = 0
 
 
-# Styles
+# Adds styles/colours
 class Styles:
     red = "\033[91m"
     green = "\033[92m"
+    blue = "\033[93m"
     reset = "\033[0m"
     bold_underline = "\033[1m\033[4m"
 
@@ -36,6 +37,29 @@ def string_checker(question, valid_ans=("yes", "no")):
         print()
 
 
+def int_checker(question):
+    while True:
+
+        error = f"{Styles.red}Please enter an integer that is 1 or higher{Styles.reset}"
+
+        to_check = input(question)
+
+        if to_check == "xxx":
+            return "exit"
+
+        try:
+            response = int(to_check)
+
+            # Checks if integer is greater than or equal to 1
+            if response <= 1:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
 # Define the instructions
 def instructions():
     print(f"{Styles.bold_underline}Instructions{Styles.reset}")
@@ -49,24 +73,34 @@ def gen_multi():
     return a, b, ans
 
 
+# Asks user if they want to read the instruction
 print("Welcome to Multiplication Quiz")
 want_instructions = string_checker("Would you like to read the instructions?")
 if want_instructions == "yes":
     instructions()
 
-
 # Game :D
 while True:
+    # Asks user math question
     a, b, ans = gen_multi()
     print(f"\nWhat is {a} * {b}")
     print(ans)
-    user_ans = input("Your answer: ")
-    user_ans = int(user_ans)
+    user_ans = int_checker("Your answer: ")
 
+    # Checks for exit code
+    if user_ans == "xxx":
+        print("Exit")
+        break
+    else:
+        user_ans = int(user_ans)
+
+    # Check if user_ans is correct
     if user_ans == ans:
         correct += 1
         print(f"You were {Styles.green}correct!{Styles.reset}")
+    elif user_ans == "exit":
+        break
     else:
         incorrect += 1
-        print(f"{Styles.red}Incorrect{Styles.reset}. The correct answer was {Styles.green}{ans}{Styles.reset}")
-    print(Styles.green, correct, Styles.red,  incorrect, Styles.reset)
+        print(f"You were {Styles.red}Incorrect{Styles.reset}. The correct answer was {Styles.green}{ans}{Styles.reset}")
+    print(Styles.green, correct, Styles.red, incorrect, Styles.reset)
